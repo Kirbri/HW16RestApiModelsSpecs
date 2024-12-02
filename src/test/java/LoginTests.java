3af49320d2b79ce47dfa076c898ddf3218b28832
Ann Kornilova <Kirbri@yandex.ru>
2024-12-02 17:28:45 +0300

    Added login and status tests

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,141 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class LoginTests {
+    /*
+     1. Make request (POST) to https://reqres.in/api/login
+     with body { "email": "eve.holt@reqres.in", "password": "cityslicka" }
+     2. Get response { "token" : "QpwL5tke4Pnpja7X4" }
+     3. Check "token" is "QpwL5tke4Pnpja7X4" and status code 200
+    */
+
+
+    @Test
+    void successfulLoginTest() {
+        String authData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
+
+        given()
+                .body(authData)
+                .log().uri()
+                .contentType(ContentType.JSON)
+            .when()
+                .post("https://reqres.in/api/login")
+            .then()
+                .log().body()
+                .log().status()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void unsuccessfulMissingPasswordTest() {
+        String authData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"\"}";
+
+        given()
+                .body(authData)
+                .log().uri()
+                .contentType(ContentType.JSON)
+            .when()
+                .post("https://reqres.in/api/login")
+            .then()
+                .log().body()
+                .log().status()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void unsuccessfulNoCurrentDataTest() {
+        String authData = "1@#!}";
+
+        given()
+                .body(authData)
+                .log().uri()
+                .contentType(ContentType.JSON)
+            .when()
+                .post("https://reqres.in/api/login")
+            .then()
+                .log().body()
+                .log().status()
+                .statusCode(400);
+    }
+
+    @Test
+    void unsuccessfulMissingEmailOrUsernameTest() {
+        String authData = "{\"password\": \"123\"}";
+
+        given()
+                .body(authData)
+                .log().uri()
+                .contentType(ContentType.JSON)
+            .when()
+                .post("https://reqres.in/api/login")
+            .then()
+                .log().body()
+                .log().status()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    void unsuccessfulUserNotFound400Test() {
+        String authData = "{\"email\": \"dfsdfsddf\", \"password\": \"12312312312\"}";
+
+        given()
+                .body(authData)
+                .log().uri()
+                .contentType(ContentType.JSON)
+            .when()
+                .post("https://reqres.in/api/login")
+            .then()
+                .log().body()
+                .log().status()
+                .statusCode(400)
+                .body("error", is("user not found"));
+    }
+
+    @Test
+    void unsuccessfulLoginNoJSON400Test() {
+        String authData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
+
+        given()
+                .body(authData)
+                .log().uri()
+            .when()
+                .post("https://reqres.in/api/login")
+            .then()
+                .log().body()
+                .log().status()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+    @Test
+    void unsuccessfulLoginNoData400Test() {
+        String authData = "";
+
+        given()
+                .body(authData)
+                .log().uri()
+            .when()
+                .post("https://reqres.in/api/login")
+            .then()
+                .log().body()
+                .log().status()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    void unsuccessfulLogin415Test() {
+        given()
+                .log().uri()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().body()
+                .log().status()
+                .statusCode(415);
+    }
+}
---
+++ src/test/java/StatusTests.java
@@ -0,0 +1,88 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+
+public class StatusTests {
+
+/*
+    1. Make request to https://selenoid.autotests.cloud/status
+    2. Get response {  "total": 20,  "used": 0,  "queued": 0,  "pending": 0, ...}
+    3.Check total is 20
+*/
+
+    @Test
+    void checkTotal20Test() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithResponseLogsTest() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithLogsTest() {
+        given()
+                .log().all()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithSomeLogsTest() {
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().body()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithStatusLogsTest() {
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().body()
+                .log().status()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkChrome100Test() {
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().body()
+                .log().status()
+                .statusCode(200)
+                .body("browsers.chrome", hasKey("100.0"));
+    }
+
+    @Test
+    void checkChrome100Firefox123Opera107Test() {
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().body()
+                .log().status()
+                .statusCode(200)
+                .body("browsers.chrome", hasKey("100.0"))
+                .body("browsers.firefox", hasKey("123.0"))
+                .body("browsers.opera", hasKey("107.0"));
+    }
+}
